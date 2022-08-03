d59bd40d01fe81b8bccd9d2e2a8171db5c828d9f
kmpark2 <98770234+kmpark2@users.noreply.github.com>
2022-08-03 11:33:46 +0900

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a temperature in Fahrenheit");
+            var userInput  = double.Parse(Console.ReadLine());
+            Console.WriteLine(TempConverter.FahrenheitToCelcius(userInput));
+            Console.WriteLine("Input a temperature in Celcius");
+            var userInput2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(TempConverter.CelciusToFahrenheit(userInput2));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fTemp)
+        {
+            var result = (fTemp - 32) / 1.8;
+            return result;
+        }
+        public static double CelciusToFahrenheit(double cTemp)
+        {
+            var result = (cTemp * 1.8) + 32;
+            return result;
+        }
+    }
+}
